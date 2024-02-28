63940ed7c4e9c3c446b0145f60c292d391f2bf2a
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2024-02-28 14:00:42 -0300

    123

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,119 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="cv">
+    <header>
+      <h1>{{ personalInfo.name }}</h1>
+      <p>{{ personalInfo.title }}</p>
+      <p>{{ personalInfo.address }}</p>
+      <p>{{ personalInfo.email }}</p>
+    </header>
+
+    <section class="experience">
+      <h2>Experiencia Laboral</h2>
+      <ul>
+        <li v-for="job in experience" :key="job.id">
+          <h3>{{ job.position }}</h3>
+          <p>{{ job.company }}, {{ job.year }}</p>
+          <p>{{ job.description }}</p>
+        </li>
+      </ul>
+    </section>
+
+    <section class="education">
+      <h2>Educación</h2>
+      <ul>
+        <li v-for="edu in education" :key="edu.id">
+          <h3>{{ edu.degree }}</h3>
+          <p>{{ edu.school }}, {{ edu.year }}</p>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      personalInfo: {
+        name: 'Tu Nombre',
+        title: 'Desarrollador Web',
+        address: 'Tu Dirección',
+        email: 'tu@email.com'
+      },
+      experience: [
+        {
+          id: 1,
+          position: 'Desarrollador Web',
+          company: 'Empresa ABC',
+          year: '2022-2024',
+          description: 'Trabajé en...'
+        },
+        // Más experiencias laborales aquí
+      ],
+      education: [
+        {
+          id: 1,
+          degree: 'Licenciatura en Informática',
+          school: 'Universidad XYZ',
+          year: '2018-2022'
+        },
+        // Más información educativa aquí
+      ]
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+#cv {
+  font-family: 'Arial', sans-serif;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f3f4f6;
+}
+
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 40px;
+}
+
+h1 {
+  font-size: 32px;
+  margin-bottom: 8px;
+}
+
+p {
+  margin: 4px 0;
+}
+
+.experience,
+.education {
+  margin-bottom: 40px;
+}
+
+h2 {
+  color: #333;
+  font-size: 24px;
+  margin-bottom: 16px;
+}
+
+ul {
+  padding: 0;
+}
+
+li {
+  margin-bottom: 20px;
+}
+
+h3 {
+  color: #555;
+  font-size: 20px;
+  margin-bottom: 8px;
+}
+
+p {
+  color: #666;
+  margin: 4px 0;
 }
 </style>
